6104a042d8a8e744e903cf4c1505eccf7768d6a2
UnderscoreAnti <UnderscoreAnti@gmail.com>
2023-07-12 01:43:02 -0600

    Fixed and finished config, started installation process.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
+import zipfile as zf
 
 
 def create_config():
-
-    appdata_dir = os.getenv('APPDATA') + "\\Godot\\app_userdata\\GDQuickInstall"
+    appdata_dir = os.getenv('APPDATA') + r"\Godot\app_userdata\GDQuickInstall"
 
     if not os.path.exists(appdata_dir):
         os.mkdir(appdata_dir)
 
-    new_config = open("settings.txt", 'w')
+    new_config = open(appdata_dir + r"\settings.txt", 'w')
 
     download_folder = input("Please enter the Godot ZIP download location\n")
     install_folder = input("Please enter the installation location\n")
@@ -21,20 +21,25 @@ def create_config():
 
 
 def get_config():
-
     settings = []
-    config_dir = os.getenv('APPDATA') + "\\Godot\\app_userdata\\GDQuickInstall\\settings.txt"
+    config_dir = os.getenv('APPDATA') + r"\Godot\app_userdata\GDQuickInstall\settings.txt"
 
     if os.path.exists(config_dir):
-        config_file = open(config_dir)
-
+        config_file = open(config_dir, "r")
     else:
         print("There was an error loading the config file...")
         create_config()
         return
 
+    # Parse the file to make the contents usable.
     for line in config_file.readlines():
-        settings.append(line)
+        raw_line = repr(line)[1:-1]
+        raw_line = raw_line.replace(r"\\", r"'\'"[1:-1])
+
+        if r"\n" in raw_line:
+            raw_line = raw_line.removesuffix(r"\n")
+
+        settings.append(raw_line)
 
     config_file.close()
     return settings
@@ -47,16 +52,13 @@ def change_config():
         print("Using 1 or 2, please choose an option to edit:")
         choose_edit = input("1.) Downloads >> {0}\n2.) Installation >> {1}\n".format(con_set[0], con_set[1]))
         new_setting = input("Please enter the new directory:\n")
-
     else:
         print("This really shouldn't happen please dear lord don't let this happen")
 
     if choose_edit == '1':
         con_set[0] = new_setting
-
     elif choose_edit == '2':
         con_set[1] = new_setting
-
     else:
         print("Please enter a valid option.")
         change_config()
@@ -68,8 +70,38 @@ def change_config():
     config_file.close()
 
 
-def get_godot_zip():
-    pass
+def get_commands():
+    out_dict = {
+        "-c": print_command_list(),
+        "-create_cfg": create_config(),
+        "-change_cfg": change_config(),
+        "-install_gd": start_installation(),
+        "-delete_gd": delete_installation(),
+    }
+
+    return out_dict
+
+
+def get_godot_zip(removable=[]):
+    settings = get_config()
+
+    size_check = []
+
+    contents = os.listdir(settings[0])
+    for tent in contents:
+        if "Godot_v" in tent and not removable and tent.endswith(".zip"):
+            print(tent)
+            size_check.append(tent)
+
+    newest_version = ""
+    for vers in size_check:
+        if newest_version == "":
+            vers == newest_version
+        else:
+            if newest_version < vers:
+                newest_version = vers
+
+    return newest_version, removable
 
 
 def move_godot_zip():
@@ -81,11 +113,30 @@ def delete_old_godot():
 
 
 def print_command_list():
-    pass
+    commands_dict = get_commands()
+
+    print("Commands:")
+    for key in commands_dict.keys():
+        print(key)
+
+    print("\n")
 
 
 def start_installation():
-    pass
+    current_version, removable = get_godot_zip()
+    check_installation(current_version, removable)
+
+
+def check_installation(curr, rem):
+    current_version_check = input("Use version '{0}' ? ('y' = yes, 'n' = no)\n".format(curr))
+
+    if current_version_check == 'y':
+        move_godot_zip()
+    elif current_version_check == 'n':
+        curr, rem = get_godot_zip(rem)
+    else:
+        print("Invalid entry, aborting and attempting operation again...")
+        start_installation()
 
 
 def delete_installation():
@@ -93,14 +144,7 @@ def delete_installation():
 
 
 def new_command():
-
-    commands_dict = {
-        "-c": print_command_list(),
-        "-create_cfg": create_config(),
-        "-change_cfg": change_config(),
-        "-install_gd": start_installation(),
-        "-delete_gd": delete_installation(),
-    }
+    commands_dict = get_commands()
 
     cmnd = input("Hello!\n")
 
@@ -109,9 +153,8 @@ def new_command():
 
     else:
         print("Please enter a valid command. '-c' will show you the command list.")
-
-
+        new_command()
 
 
 if __name__ == '__main__':
-    pass
+    start_installation()
